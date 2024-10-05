a4c4df523a07752d41fa89a332a3028ecf1e9afe
selam-Tekleab <selam.tekleab6@gmail.com>
2024-10-05 17:19:17 -0500

    initial commit

---
+++ complexMapping/src/main/java/miu/edu/complexmapping/ComplexMappingApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.complexmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexMappingApplication.class, args);
+	}
+
+}
---
+++ complexMapping/src/test/java/miu/edu/complexmapping/ComplexMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.complexmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplexMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
